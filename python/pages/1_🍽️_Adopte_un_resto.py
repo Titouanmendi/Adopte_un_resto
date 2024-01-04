be05c6b76ea56ba8200ffa564f440bb461b04f2f
Titouan <titouan.mendiharat@cit.io>
2024-01-04 15:38:05 +0100

    [Verified] Merge

--- python/pages/1_🍽️_Adopte_un_resto.py
+++ python/pages/1_🍽️_Adopte_un_resto.py
@@ -1,62 +1,92 @@
 import streamlit as st
 import pandas as pd
-from streamlit_dynamic_filters import DynamicFilters
 from ast import literal_eval
+
+import folium
+from folium.plugins import FastMarkerCluster
+from streamlit_folium import folium_static
 
 # -- Set page config
 apptitle = 'Adopte un resto'
 
 st.set_page_config(page_title=apptitle, page_icon=":fork_knife_plate:")
 
-df = pd.read_csv("data/restaurants.csv")
-
 # -- Default detector list
 detectorlist = ['H1','L1', 'V1']
 
-# Title the app
-st.title('Liste de restaurants')
 
-st.markdown("""
- * Choisissez un restaurant grâce aux filtres à gauche
-""")
+# -- Sidebar
+
+df = pd.read_csv("data/restaurants.csv")
+df['food_constraints']=df['food_constraints'].apply(literal_eval)
+df['food_type']=df['food_type'].apply(literal_eval)
 
     
 st.sidebar.markdown("## Filtres")
-time_available = st.sidebar.slider('Distance (minutes)', 1, 30, 1)  # min, max, default
 
+time_available = st.sidebar.slider('Distance (minutes)', 0, 30, 1)  
 
+df_food_contraints = pd.DataFrame({
+    'food_constraints': df['food_constraints'].explode().unique()
+})
+food_constraints = st.sidebar.multiselect(
+    'Régime', options=df_food_contraints, default=[]
+)
 
-# literal_eval
-
-# df[''].eexplode()
-
-
-
-
-
-# dfRegime = pd.DataFrame({
-#     'Regime': donnees_regime
+# df_food_type = pd.DataFrame({
+#     'food_type': df['food_type'].explode().unique()
 # })
+# food_types = st.sidebar.multiselect(
+#     'Pas envie', options=df_food_type, default=[]
+# )
 
-# dfPlats = pd.DataFrame({
-#     'platsBof': donnee_platsPasEnvie
-# })
+price = st.sidebar.radio(
+    "Prix",
+    ("€", "€€", "€€€"),
+    help="(€ (0-10), €€ (10-20) et €€€ (20-plus)",
+)
 
-
-# dynamic_filters = DynamicFilters(dfRegime , filters=['food_types'])
-# dynamic_filters.display_filters(location='sidebar')
-# df_filtered = dynamic_filters.filter_df()
-
-
-# dynamic_filters_1 = DynamicFilters(dfPlats , filters=['platsBof'])
-# dynamic_filters_1.display_filters(location='sidebar')
-# df_filtered = dynamic_filters_1.filter_df()
+price_mapping = {"€":1, "€€":2, "€€€":3}
+price_value = price_mapping[price]
 
 
+filtered_df = df[df['dist_minutes']<=time_available].assign(**{
+    'all_constraints_validated': lambda df: df['food_constraints'].apply(lambda c: set(food_constraints) <= set(c))
+})
+filtered_df = filtered_df[filtered_df['all_constraints_validated']]
 
-check1 = st.sidebar.button("Manger !")
+filtered_df = filtered_df[filtered_df['price']<=price_value]
+
+print(filtered_df)
+  
+
+display_restaurants = st.sidebar.button("Manger !")
 
 
+# -- Main page
 
+LYBY = (48.8432804, 2.3720586)
 
+show_map = st.toggle("Liste / Carte", value=True)
 
+# df = pd.read_csv("data/restaurants.csv")
+if show_map:
+    st.markdown("### Carte des restaurants")
+    lat_lon = filtered_df.apply(lambda row: (row["lat"], row["lon"]), axis=1).to_list()
+    m = folium.Map(location=LYBY, tiles="Cartodb Positron", zoom_start=14)
+    for index, row in filtered_df.iterrows():
+        popup_content = (
+            f'<h3>{row["name"]}</h3>'
+            f'<p><b>Food Type:</b> {row["food_type"]}</p>'
+            f'<p><b>Price:</b> {row["price"]}</p>'
+            f'<p><b>Distance (minutes):</b> {row["dist_minutes"]}</p>'
+            f'<p><b>Food Constraints:</b> {row["food_constraints"]}</p>'
+        )
+        folium.Marker(
+            [row["lat"], row["lon"]], popup=folium.Popup(popup_content, max_width=300)
+        ).add_to(m)
+    FastMarkerCluster(data=lat_lon).add_to(m)
+
+    folium_static(m, width=800, height=500)
+else:
+    st.markdown("### Liste des restaurants")
