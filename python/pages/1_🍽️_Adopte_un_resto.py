fa0bfa8c156b0ed094cff8b056dab45c8f402d10
Alice Breton <alice@cit.io>
2024-01-04 15:24:26 +0100

    [Verified] filters

--- python/pages/1_🍽️_Adopte_un_resto.py
+++ python/pages/1_🍽️_Adopte_un_resto.py
@@ -8,10 +8,55 @@ apptitle = 'Adopte un resto'
 
 st.set_page_config(page_title=apptitle, page_icon=":fork_knife_plate:")
 
-df = pd.read_csv("data/restaurants.csv")
-
 # -- Default detector list
 detectorlist = ['H1','L1', 'V1']
+
+
+# -- Sidebar
+
+df = pd.read_csv("data/restaurants.csv")
+df['food_constraints']=df['food_constraints'].apply(literal_eval)
+df['food_type']=df['food_type'].apply(literal_eval)
+
+    
+st.sidebar.markdown("## Filtres")
+
+time_available = st.sidebar.slider('Distance (minutes)', 0, 30, 1)  
+
+df_food_contraints = pd.DataFrame({
+    'food_constraints': df['food_constraints'].explode().unique()
+})
+food_constraints = st.sidebar.multiselect(
+    'Régime', options=df_food_contraints, default=[]
+)
+
+# df_food_type = pd.DataFrame({
+#     'food_type': df['food_type'].explode().unique()
+# })
+# food_types = st.sidebar.multiselect(
+#     'Pas envie', options=df_food_type, default=[]
+# )
+
+price = st.sidebar.radio(
+    "Prix",
+    ("€", "€€", "€€€"),
+    help="(€ (0-10), €€ (10-20) et €€€ (20-plus)",
+)
+
+price_mapping = {"€":1, "€€":2, "€€€":3}
+price_value = price_mapping[price]
+
+
+filtered_df = df[df['dist_minutes']<=time_available].assign(**{
+    'all_constraints_validated': lambda df: df['food_constraints'].apply(lambda c: set(food_constraints) <= set(c))
+})
+filtered_df = filtered_df[filtered_df['all_constraints_validated']]
+
+filtered_df = filtered_df[filtered_df['price']<=price_value]
+  
+
+display_restaurants = st.sidebar.button("Manger !")
+
 
 # Title the app
 st.title('Liste de restaurants')
@@ -19,44 +64,6 @@ st.title('Liste de restaurants')
 st.markdown("""
  * Choisissez un restaurant grâce aux filtres à gauche
 """)
-
-    
-st.sidebar.markdown("## Filtres")
-time_available = st.sidebar.slider('Distance (minutes)', 1, 30, 1)  # min, max, default
-
-
-
-# literal_eval
-
-# df[''].eexplode()
-
-
-
-
-
-# dfRegime = pd.DataFrame({
-#     'Regime': donnees_regime
-# })
-
-# dfPlats = pd.DataFrame({
-#     'platsBof': donnee_platsPasEnvie
-# })
-
-
-# dynamic_filters = DynamicFilters(dfRegime , filters=['food_types'])
-# dynamic_filters.display_filters(location='sidebar')
-# df_filtered = dynamic_filters.filter_df()
-
-
-# dynamic_filters_1 = DynamicFilters(dfPlats , filters=['platsBof'])
-# dynamic_filters_1.display_filters(location='sidebar')
-# df_filtered = dynamic_filters_1.filter_df()
-
-
-
-check1 = st.sidebar.button("Manger !")
-
-
 
 
 
