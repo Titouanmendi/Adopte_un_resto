45ba79d509e1784f795b0f3c9e7a579b7dc3a5ac
Titouan <titouan.mendiharat@cit.io>
2024-01-04 15:36:05 +0100

    [Verified] added word_to_better_word

--- python/utils.py
+++ python/utils.py
@@ -1,20 +1,29 @@
 import ast
 
-import pandas as pd
+word_to_emoji_mapping = {
+    "VEGE": ":pousse:",
+    "VEGAN": ":pousse::interdit::verre_de_lait::cuisse_de_volaille:",
+    "HALAL": ":interdit::cochon2:",
+    "LACTOSE_FREE": ":interdit::verre_de_lait:",
+    "GLUTEN_FREE": ":interdit::pain:",
+    "SUSHI": ":sushi:",
+    "PIZZA": ":pizza:",
+    "BURGER": ":hamburger:",
+}
+
+word_to_better_word = {
+    "VEGE": "Vegetarien",
+    "VEGAN": "Vegan",
+    "HALAL": "Halal",
+    "LACTOSE_FREE": "Sans lactose",
+    "GLUTEN_FREE": " Sans gluten",
+    "SUSHI": "Sushi",
+    "PIZZA": "Pizza",
+    "BURGER": "Burger",
+}
 
 
 def convert_words_to_emojis(input_string):
-    word_to_emoji_mapping = {
-        "VEGE": ":pousse:",
-        "VEGAN": ":pousse::interdit::verre_de_lait::cuisse_de_volaille:",
-        "HALAL": ":interdit::cochon2:",
-        "LACTOSE_FREE": ":interdit::verre_de_lait:",
-        "GLUTEN_FREE": ":interdit::pain:",
-        "SUSHI": ":sushi:",
-        "PIZZA": ":pizza:",
-        "BURGER": ":hamburger:",
-    }
-
     words = ast.literal_eval(input_string)
 
     emojis = [word_to_emoji_mapping.get(word, word) for word in words]
