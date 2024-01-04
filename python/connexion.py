dd0dc9a1952355073e42b5c810631bec639269c6
Esther <esther.beugre@cit.io>
2024-01-04 16:47:07 +0100

    [Verified] ajout emoji

--- python/connexion.py
+++ python/connexion.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
 
-st.write("# Connecte-toi pour adopter ton restaurant! 😋")
+st.write("# Connecte-toi pour adopter ton restaurant! 🍽😋")
+
 
 def authentification_et_redirection():
     # Champ d'entrée pour l'identifiant
@@ -16,14 +16,13 @@ def authentification_et_redirection():
     mdp_utilisateur = st.text_input("Mot de passe", type="password")
 
     # Bouton pour soumettre le formulaire et rediriger vers une autre page
-    #bouton_connexion = st.button("VITE J'AI FAIM")
-    
-    st.link_button("VITE J'AI FAIM","http://localhost:8501/Adopte_un_resto")
-        # # Redirection vers une autre page (remplacez l'URL ci-dessous par celle de votre page d'authentification)
-        # #st.markdown("<script>window.location.href = 'http://localhost:8501/Adopte_un_resto';</script>", unsafe_allow_html=True)
-        # st.markdown('<a href="http://localhost:8501/Adopte_un_resto" target="_blank">Rediriger</a>', unsafe_allow_html=True)
+    # bouton_connexion = st.button("VITE J'AI FAIM")
+
+    st.link_button("VITE J'AI FAIM", "http://localhost:8501/Adopte_un_resto")
+    # # Redirection vers une autre page (remplacez l'URL ci-dessous par celle de votre page d'authentification)
+    # #st.markdown("<script>window.location.href = 'http://localhost:8501/Adopte_un_resto';</script>", unsafe_allow_html=True)
+    # st.markdown('<a href="http://localhost:8501/Adopte_un_resto" target="_blank">Rediriger</a>', unsafe_allow_html=True)
 
 
 # Appeler la fonction d'authentification et redirection
 authentification_et_redirection()
-
